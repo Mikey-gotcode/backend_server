a3951f79d5458c4faeff6bd8ef2fa6e8c98e21fe
dedanl <123885283+dedanl@users.noreply.github.com>
2024-08-06 18:06:11 +0300

    first commit

--- server.js
+++ server.js
@@ -11,3 +11,6 @@ httpServer.listen(5001,()=>console.log('server is live'))
 app.use('/api',(_,res)=>{
     res.json({message:'hello world',status:200})
 })
+app.use('/api',(_,res)=>{
+    res.json({message:'hello world',status:200})
+})
