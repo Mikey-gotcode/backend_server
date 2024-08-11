5e7d4832d99e1766cabddebfa2e2cae057429c2b
dedanl <123885283+dedanl@users.noreply.github.com>
2024-08-11 22:55:36 +0300

    ckeck for whether render logs server info

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ Server.listen(5001,()=>console.log('server is live'))
 
 
 app.use('/api',(_,res)=>{
+    console.log('back end reached')
     res.json({message:'hello world',status:200})
 })
 app.use('/api',(_,res)=>{
